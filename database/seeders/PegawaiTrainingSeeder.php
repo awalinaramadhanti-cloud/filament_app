01e07a16ec92bec18120a34765f0589497419153
awalina rizki ramadhanti <awalinaramadhanti@gmail.com>
2026-06-04 14:42:33 +0700

    Praktikum-12

--- database/seeders/PegawaiTrainingSeeder.php
+++ database/seeders/PegawaiTrainingSeeder.php
@@ -1,32 +1,14 @@
 <?php
-namespace Database\Seeders;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-class PegawaiTrainingSeeder extends Seeder
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class PegawaiTraining extends Model
 {
-/**
-* Run the database seeds.
-*/
-public function run(): void
-{
-//
-DB::table('pegawai_training')->insert([
-[
-'pegawai_id' => 1,
-'training_id' => 1,
-'status' => 'Selesai',
-],
-[
-'pegawai_id' => 2,
-'training_id' => 2,
-'status' => 'Mengikuti',
-],
-[
-'pegawai_id' => 3,
-'training_id' => 3,
-'status' => 'Terdaftar',
-],
-]);
-}
+    protected $table = 'pegawai_training';
+    protected $fillable = [
+        'pegawai_id', 
+        'training_id', 
+        'status',
+    ];
 }
